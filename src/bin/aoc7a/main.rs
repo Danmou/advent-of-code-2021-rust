521a34f8c85c4f48bdaaa84266ac9e9699b2ddc8
Daniel Mouritzen <daniel.mouritzen@voliro.com>
2021-12-07 20:48:08 +0100

    Day 7

---
+++ src/bin/aoc7a/main.rs
@@ -0,0 +1,39 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn read_input() -> Vec<i32> {
+    let file = File::open("inputs/7.txt");
+
+    let file = match file {
+        Ok(file) => file,
+        Err(error) => panic!("Problem opening the file: {:?}", error),
+    };
+
+    let br = BufReader::new(file);
+
+    br.lines().next().unwrap().unwrap().split(",").map(|n| n.parse().unwrap()).collect()
+}
+
+fn find_least_fuel_to_align(start_positions: Vec<i32>) -> i64 {
+    let &min = start_positions.iter().min().unwrap();
+    let &max = start_positions.iter().max().unwrap();
+    let mut best_position = -1;
+    let mut best_fuel_used = i64::MAX;
+    for pos in min..max {
+        let mut fuel_used = 0;
+        for &crab_pos in start_positions.iter() {
+            fuel_used += (crab_pos - pos).abs() as i64;
+        }
+        if fuel_used < best_fuel_used {
+            best_position = pos;
+            best_fuel_used = fuel_used;
+        }
+    }
+    best_fuel_used
+}
+
+fn main() {
+    let input = read_input();
+    let num = find_least_fuel_to_align(input);
+    println!("{}", num);
+}
